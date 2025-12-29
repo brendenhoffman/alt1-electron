75055f99f9b1cb4cf78384b334633ff61d1c1127
Brenden Hoffman <bhessential1@fastmail.com>
2025-12-28 22:34:51 -0600

    Correct webpack config and preload path to expose Alt1 API in renderer
    Signed-off-by: Brenden Hoffman <bhessential1@fastmail.com>

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,6 +1,59 @@
 const path = require("path");
 
 /** @type {import("webpack").Configuration} */
+const common = {
+  resolve: {
+	extensions: [".wasm", ".tsx", ".ts", ".mjs", ".jsx", ".js", ".json"],
+	alias: {
+	  "electron/main": "electron",
+	  "electron/renderer": "electron",
+	  "electron/common": "electron",
+	},
+  },
+  module: {
+	parser: { javascript: { commonjsMagicComments: true } },
+	rules: [
+	  { test: /\.(ts|tsx)$/, use: [{ loader: "ts-loader" }] },
+	  { test: /\.css$/, use: ["style-loader", "css-loader"] },
+	  { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+
+	  { test: /\.data\.png$/i, use: [{ loader: "alt1/imagedata-loader" }] },
+	  {
+		test: /\.(png|jpg|gif)$/i,
+		exclude: /\.data\.png$/i,
+		use: [
+		  {
+			loader: "url-loader",
+			options: {
+			  limit: 8192,
+			  esModule: false,
+			  name: "[path][name].[ext]",
+			},
+		  },
+		],
+	  },
+
+	  {
+		test: /\.json$/,
+		type: "javascript/auto",
+		use: [{ loader: "json-loader" }],
+	  },
+	  {
+		test: /\.fontmeta\.json$/,
+		type: "javascript/auto",
+		use: [{ loader: "alt1/font-loader" }],
+	  },
+
+	  {
+		test: /\.html$/,
+		use: [
+		  { loader: "file-loader", options: { name: "[path][name].[ext]" } },
+		],
+	  },
+	],
+  },
+};
+
 module.exports = (env = {}) => {
   const prod = !!(
 	env.prod ??
@@ -8,99 +61,57 @@ module.exports = (env = {}) => {
 	(env.mode ? env.mode === "prod" : false)
   );
 
-  return {
-	context: path.resolve(__dirname, "./src/"),
-	mode: prod ? "production" : "development",
-	target: "web",
-	node: false,
+  const mode = prod ? "production" : "development";
+  const devtool = prod ? "source-map" : "eval-source-map";
 
-	entry: {
-	  alt1lite: "./main.ts",
-	  "appframe/index": "./appframe/index.tsx",
-	  "appframe/alt1api": "./appframe/alt1api.ts",
-	  "overlayframe/index": "./overlayframe/index.tsx",
-	  "settings/index": "./settings/index.tsx",
-	  "tooltip/index": "./tooltip/index.tsx",
-	  "tests/index": "./tests/index.ts",
-	},
-
-	output: {
-	  path: path.resolve(__dirname, "dist"),
-	  filename: "[name].bundle.js",
-	  chunkFilename: prod ? "[name]_[chunkhash].bundle.js" : "[name].bundle.js",
-	  globalObject: "(typeof self!='undefined'?self:this)",
-	},
-
-	resolve: {
-	  extensions: [".wasm", ".tsx", ".ts", ".mjs", ".jsx", ".js", ".json"],
-	},
-
-	devtool: prod ? "source-map" : "eval-source-map",
-
-	externals: [
-	  { canvas: { root: "null", commonjs: null, commonjs2: null, amd: null } },
-	  { sharp: { root: "null", commonjs: null, commonjs2: null, amd: null } },
-	],
-
-	module: {
-	  parser: {
-		javascript: { commonjsMagicComments: true },
-	  },
-	  rules: [
-		{
-		  test: /\.(ts|tsx)$/,
-		  use: [{ loader: "ts-loader" }],
-		},
-		{
-		  test: /\.css$/,
-		  use: ["style-loader", "css-loader"],
-		},
-		{
-		  test: /\.scss$/,
-		  use: ["style-loader", "css-loader", "sass-loader"],
-		},
-
-		{
-		  test: /\.data\.png$/i,
-		  use: [{ loader: "alt1/imagedata-loader" }],
-		},
-		{
-		  test: /\.(png|jpg|gif)$/i,
-		  exclude: /\.data\.png$/i,
-		  use: [
-			{
-			  loader: "url-loader",
-			  options: {
-				limit: 8192,
-				esModule: false,
-				name: "[path][name].[ext]",
-			  },
-			},
-		  ],
-		},
-
-		{
-		  test: /\.json$/,
-		  type: "javascript/auto",
-		  use: [{ loader: "json-loader" }],
-		},
-		{
-		  test: /\.fontmeta\.json$/,
-		  type: "javascript/auto",
-		  use: [{ loader: "alt1/font-loader" }],
-		},
-		{
-		  test: /\.html$/,
-		  use: [
-			{ loader: "file-loader", options: { name: "[path][name].[ext]" } },
-		  ],
-		},
-	  ],
-	},
-
-	optimization: {
-	  minimize: prod,
-	  moduleIds: prod ? "natural" : "named",
-	},
+  const output = {
+	path: path.resolve(__dirname, "dist"),
+	filename: "[name].bundle.js",
+	chunkFilename: prod ? "[name]_[chunkhash].bundle.js" : "[name].bundle.js",
+	globalObject: "(typeof self!='undefined'?self:this)",
   };
+
+  const externalsCommon = {
+	electron: "commonjs2 electron",
+	"electron/main": "commonjs2 electron",
+	"electron/renderer": "commonjs2 electron",
+	"electron/common": "commonjs2 electron",
+	fs: "commonjs2 fs",
+	path: "commonjs2 path",
+	os: "commonjs2 os",
+	canvas: "commonjs2 canvas",
+	sharp: "commonjs2 sharp",
+  };
+
+  return [
+	{
+	  // Main process bundle
+	  ...common,
+	  mode,
+	  devtool,
+	  target: "electron-main",
+	  context: path.resolve(__dirname, "./src/"),
+	  entry: { alt1lite: "./main.ts" },
+	  output,
+	  externals: externalsCommon,
+	},
+	{
+	  // Renderer bundles
+	  ...common,
+	  mode,
+	  devtool,
+	  target: "electron-renderer",
+	  context: path.resolve(__dirname, "./src/"),
+	  entry: {
+		"appframe/index": "./appframe/index.tsx",
+		"appframe/alt1api": "./appframe/alt1api.ts",
+		"overlayframe/index": "./overlayframe/index.tsx",
+		"settings/index": "./settings/index.tsx",
+		"tooltip/index": "./tooltip/index.tsx",
+		"tests/index": "./tests/index.ts",
+	  },
+	  output,
+	  externals: externalsCommon,
+	},
+  ];
 };
