ac6c9b64464ee66da63c672ec36da44042f73442
Brenden Hoffman <13664514+brendenhoffman@users.noreply.github.com>
2026-02-10 12:18:47 -0600

    Create electron-builder.ci.cjs

---
+++ electron-builder.ci.cjs
@@ -0,0 +1,19 @@
+// electron-builder.ci.cjs
+const pkg = require("./package.json");
+
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+const repo = (process.env.GITHUB_REPOSITORY || "").split("/")[1];
+
+module.exports = {
+	// start with normal electron-builder config
+	...(pkg.build || {}),
+
+	// override only publish target to the repo running the workflow
+	publish: [
+	{
+		provider: "github",
+		owner,
+		repo,
+	},
+	],
+};
